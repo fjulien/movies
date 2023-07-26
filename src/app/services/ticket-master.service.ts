6928a22b506ae51b5a5f88f08234ddf7e00034bf
fjulien <fettingerjulien@yahoo.fr>
2023-07-26 15:30:11 +0000

    draft

--- src/app/services/ticket-master.service.ts
+++ src/app/services/ticket-master.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,12 +6,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class TicketMasterService {
+  private readonly apiKey:string = process.env['NX_KEY_API_TICKET_MASTER']!;
 
-  constructor(private readonly httpClient :HttpClient) {
-    
-   }
+  constructor(private readonly httpClient: HttpClient) {
 
-   search(): Observable<any>{
-      return this.httpClient.get(`https://app.ticketmaster.com/discovery/v2/events?apikey=&postalCode=33000&locale=fr&city=Bordeaux`);
-   }
+  }
+
+  search(): Observable<any> {
+    const params = new HttpParams()
+    .append('apikey', this.apiKey)
+    .append('postalCode', 33000)
+    .append('local', 'fr');
+    return this.httpClient.get(`https://app.ticketmaster.com/discovery/v2/events?apikey=&postalCode=33000&locale=fr&city=Bordeaux`);
+  }
 }
